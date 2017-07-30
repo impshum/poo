93a2dc48d8ecac55f3c598b5c352f30efe0a56de
impshum <impshum@impshum.co.uk>
2017-07-30 19:46:59 +0100

    poo

---
+++ index.php
@@ -0,0 +1,10 @@
+<style>
+/* POO */
+html {
+  background: url(bg.gif) no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+</style>
